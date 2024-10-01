5f1b5f32149c8eeeca600bc95da3029a5a6df2c1
zoe0716310 <roger031460@gmail.com>
2024-10-01 20:06:32 +0800

    1001

--- ParseLog.sh
+++ ParseLog.sh
@@ -2,6 +2,6 @@
 
 # 搜尋以"Receive"開頭的行並匯出成Receive.txt
 
-awk '/^[0-9]+ : /' log.txt > Receive.txt
+awk '/^[0-9]+ : /' logs.txt > Receive.txt
 
 echo "已匯出以 number : 開頭的行到 Receive.txt"
--- createSh_ATP.sh
+++ createSh_ATP.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # 定义数组
-mean_bw=(30)  # 用你的实际数值替换这些示例值
-deltas=(1 2 3 4 5)
+mean_bw=(20)  # 用你的实际数值替换这些示例值
+deltas=(1)
 seed=(1 2 3 4 5 6 7 8 9 10)
-bandwidth=(80)
+bandwidth=(40)
 
 # 遍历数组组合
 for mean_rate in "${mean_bw[@]}"; do
@@ -21,7 +21,7 @@ for mean_rate in "${mean_bw[@]}"; do
         fi
       
         # 生成命令
-        command="./ns3 run fifth -- --mean=$mean_rate --standard_deviation=$delta --outputPcapDir=$dir --outgoingBW=$bw --seed=$s --timeout=5 > ${dir}/seed${s}.txt &"
+        command="./ns3 run fifth -- --mean=$mean_rate --standard_deviation=$delta --outputPcapDir=$dir --outgoingBW=$bw --seed=$s --timeout=4 > ${dir}/seed${s}.txt &"
         # command="./ns3 run fifth -- --bigN=$mean_rate --alpha=$delta --outputPcapDir=$dir --outgoingBW=$bw --seed=$s > ${dir}/seed${s}.txt &"
 
         # 打印或执行命令
